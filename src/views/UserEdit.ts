d07bce323841a6857a6fcda5bdeec687bfd032c8
Felipe Gutiérrez <pipo@pop-os.localdomain>
2021-09-15 20:53:29 -0300

    Created userEdits and bind usershow, UserEdit with userForm

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,1 +1,30 @@
-export class UserEdit {}
+import { UserProps } from '../interfaces/UserProps'
+import { User } from '../models/User'
+import { UserForm } from './UserForm'
+import { UserShow } from './UserShow'
+import { View } from './View'
+
+export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '#user-show',
+      userForm: '#user-form',
+    }
+  }
+
+    onRender(): void {
+      const userShow = new UserShow(this.regions.userShow, this.model)
+      const userForm = new UserForm(this.regions.userForm, this.model)
+      userShow.render()
+      userForm.render()
+    }
+
+  template(): string {
+    return `
+            <div>
+                <div id="user-show"></div>
+                <div id="user-form"></div>
+            </div>
+        `
+  }
+}
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,12 +1,22 @@
 import { Model } from '../models/Model'
 
 export abstract class View<T extends Model<K>, K> {
-  constructor(public parent: HTMLElement, public model: T) {
+  regions: { [key: string]: Element } = {}
+
+  constructor(public parent: Element, public model: T) {
     this.bindModel()
   }
 
-  abstract eventsMap(): { [key: string]: () => void }
   abstract template(): string
+
+  // default implemntation
+  regionsMap(): { [key: string]: string } {
+    return {}
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {}
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -25,12 +35,32 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap()
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key]
+      const element = fragment.querySelector(selector)
+
+      if (element) {
+        this.regions[key] = element
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = ''
 
     const templateElement = document.createElement('template')
     templateElement.innerHTML = this.template()
     this.bindEvents(templateElement.content)
+
+    this.mapRegions(templateElement.content)
+
+    this.onRender()
+
     this.parent.appendChild(templateElement.content)
   }
 }
